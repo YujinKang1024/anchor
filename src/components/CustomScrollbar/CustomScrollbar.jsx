b8ec10c0466da35b862b004fca372f4c78d3ea12
YujinKang1024 <yujinkang1024@gmail.com>
2024-07-16 04:51:01 +0900

    fix: 스크롤 이벤트에 따른 boat 이동이 초기 위치를 기준으로 이루어지도록 수정, 이동 간격 조정

--- src/components/CustomScrollbar/CustomScrollbar.jsx
+++ src/components/CustomScrollbar/CustomScrollbar.jsx
@@ -7,10 +7,23 @@ import {
 } from './CustomScrollbar.styles';
 
 export default function CustomScrollbar({ boatRef }) {
-  const scrollRef = useRef();
   const [currentScrollRef, setCurrentScrollRef] = useState(null);
   const [thumbPosition, setThumbPosition] = useState(0);
+
+  const scrollRef = useRef();
+  const initialBoatPositionRef = useRef({ x: 780, y: -500.5, z: 520 });
+
   const virtualScrollHeight = 8000;
+
+  useEffect(() => {
+    if (boatRef.current) {
+      initialBoatPositionRef.current = {
+        x: boatRef.current.position.x,
+        y: boatRef.current.position.y,
+        z: boatRef.current.position.z,
+      };
+    }
+  }, [boatRef]);
 
   const handleScroll = useCallback(() => {
     if (!scrollRef.current) return;
@@ -19,23 +32,27 @@ export default function CustomScrollbar({ boatRef }) {
     const clientHeight = scrollRef.current.clientHeight;
     const scrollHeight = scrollRef.current.scrollHeight;
 
-    const newThumbPosition = Math.min(
-      (scrollTop / (scrollHeight - clientHeight)) * 100,
-      100,
-    );
+    const scrollRatio = scrollTop / (scrollHeight - clientHeight);
+    const sensitivity = 0.8;
+
+    const newThumbPosition = Math.min(scrollRatio * 100, 100);
     setThumbPosition(newThumbPosition);
 
     if (boatRef.current) {
-      const boatMaxDistanceX = 1000;
-      const boatMaxDistanceZ = 500;
+      const boatMaxDistanceX = 100;
+      const boatMaxDistanceZ = 50;
       const boatPositionX =
-        (scrollTop / (scrollHeight - clientHeight)) * boatMaxDistanceX;
+        (scrollTop / (scrollHeight - clientHeight)) *
+        boatMaxDistanceX *
+        sensitivity;
       const boatPositionZ =
-        (scrollTop / (scrollHeight - clientHeight)) * boatMaxDistanceZ;
+        (scrollTop / (scrollHeight - clientHeight)) *
+        boatMaxDistanceZ *
+        sensitivity;
       boatRef.current.position.set(
-        boatPositionX,
-        boatRef.current.position.y,
-        boatPositionZ,
+        initialBoatPositionRef.current.x + boatPositionX,
+        initialBoatPositionRef.current.y,
+        initialBoatPositionRef.current.z + boatPositionZ,
       );
     }
   }, [boatRef]);
