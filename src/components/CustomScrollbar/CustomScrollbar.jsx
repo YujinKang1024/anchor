8760ac46d1b6265f6a004f61752fd954386c8059
YujinKang1024 <yujinkang1024@gmail.com>
2024-07-16 21:51:38 +0900

    fix: scrollRef.current.scrollHeight 사용하여 thumb의 top이 50 이상으로 증가하지 않는 문제 수정

--- src/components/CustomScrollbar/CustomScrollbar.jsx
+++ src/components/CustomScrollbar/CustomScrollbar.jsx
@@ -10,7 +10,7 @@ export default function CustomScrollbar({ boatRef, pathPoints }) {
   const scrollRef = useRef();
   const initialBoatPositionRef = useRef({ x: 780, y: -500.5, z: 520 });
 
-  const virtualScrollHeight = 10000;
+  const virtualScrollHeight = 50000;
 
   useEffect(() => {
     if (boatRef.current) {
@@ -27,17 +27,20 @@ export default function CustomScrollbar({ boatRef, pathPoints }) {
 
     const scrollTop = scrollRef.current.scrollTop;
     const clientHeight = scrollRef.current.clientHeight;
-    const scrollHeight = virtualScrollHeight;
+    const scrollHeight = scrollRef.current.scrollHeight;
 
     const scrollRatio = scrollTop / (scrollHeight - clientHeight);
-    // const sensitivity = 0.8;
     const newThumbPosition = Math.min(scrollRatio * 100, 100);
+
     setThumbPosition(newThumbPosition);
 
     if (boatRef.current) {
-      const pointIndex = Math.floor(scrollRatio * (pathPoints.length - 1)); // 현재 스크롤 위치에 해당하는 경로 상의 인덱스 점 계산 (현재 위치에 가장 가까운 경로 점)
-      const nextPointIndex = Math.min(pointIndex + 1, pathPoints.length - 1); // 다음 스크롤 위치에 해당하는 인덱스 점 (pathPoints.length - 1 보다 커지지 않게 함)
-      const lerpFactor = (scrollRatio * (pathPoints.length - 1)) % 1; // 보간 비율. 점과 점 사이의 어느 위치에 있어야 하는지 스크롤 비율에 기반하여 계산함
+      const pathLength = pathPoints.length - 1;
+      const scaledScrollRatio = scrollRatio * pathLength;
+
+      const pointIndex = Math.floor(scaledScrollRatio);
+      const nextPointIndex = (pointIndex + 1) % pathPoints.length;
+      const lerpFactor = scaledScrollRatio % 1;
 
       const currentPoint = pathPoints[pointIndex];
       const nextPoint = pathPoints[nextPointIndex];
@@ -45,15 +48,7 @@ export default function CustomScrollbar({ boatRef, pathPoints }) {
       const newPosition = new THREE.Vector3().lerpVectors(currentPoint, nextPoint, lerpFactor);
 
       boatRef.current.position.set(newPosition.x, -500.5, newPosition.y);
-
-      console.log(newPosition);
     }
-
-    console.log('scrollTop:', scrollTop);
-    console.log('clientHeight:', clientHeight);
-    console.log('virtualScrollHeight:', virtualScrollHeight);
-    console.log('scrollRatio:', scrollRatio);
-    console.log('newThumbPosition:', newThumbPosition);
   }, [boatRef, pathPoints]);
 
   useEffect(() => {
